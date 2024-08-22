84b2aa1a746b190c5b202aeb54086c0efbd10704
ChidiEbube123 <142165422+ChidiEbube123@users.noreply.github.com>
2024-08-22 13:10:33 +0100

    changed medi a confgig

--- a_core/settings.py
+++ a_core/settings.py
@@ -113,7 +113,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media' 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -150,10 +154,8 @@ AWS_DEFAULT_ACL = None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+
 
 STORAGES = {
     "default": {
